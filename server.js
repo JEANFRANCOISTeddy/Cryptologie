c779b5c8760b42ca546af0927cd05e41cb4e0dcc
Teddy <jfrancoisteddy@gmail.com>
2021-02-02 10:30:42 +0100

    Change path

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const path = require('path');
 const port = 3000;
 
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/templates/index.html'));
+    res.sendFile(path.join(__dirname+'/src/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 
 app.get('/about',function(req,res){
-    res.sendFile(path.join(__dirname+'/templates/about.html'));
+    res.sendFile(path.join(__dirname+'/src/about.html'));
 });
 
 app.get('/sitemap',function(req,res){
-    res.sendFile(path.join(__dirname+'/templates/sitemap.html'));
+    res.sendFile(path.join(__dirname+'/src/sitemap.html'));
 });
 
 app.listen(port, () => {
